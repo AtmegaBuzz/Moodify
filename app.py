724d05b55530f6c23a2595a0f1fd21c4a01a9fbd
swapnil shinde <swapnilshinde9382@gmail.com>
2022-03-06 08:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask , request,render_template,flash,redirect
+from flask import Flask , request,render_template,flash,redirect, url_for
 from main import run_classifier
 import os
 import random
@@ -8,13 +8,19 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = os.path.join(os.getcwd(),"images")
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-token = ""
+#token = ""
 
 songs = {
-    "Happy":"4PY9rbCQSo2JvGdgREogIe",
-    "Sad":"4YOfhHpjPB0tq29NPpDY3F",
-    "Surprise":"7vatYrf39uVaZ8G2cVtEik"
+
+    'Disgust':"7zpZFvJpcZC2uYgdSZNfuA",
+    'Fear':"405PXg1fJunIlSo75L78Kb",
+    'Happy':"4PY9rbCQSo2JvGdgREogIe",
+    'Neutral':"3C7FXzgUwDq7hY68lW5B2d", 
+    'Sad':"4YOfhHpjPB0tq29NPpDY3F", 
+    'Surprise':"7vatYrf39uVaZ8G2cVtEik"
 }
+
+@app.route('/',methods=["GET","POST"])
 def home():
     if request.method=="POST":
        
@@ -29,7 +35,9 @@ def home():
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             image_path = os.path.join(UPLOAD_FOLDER,filename)
             mood = run_classifier(image_path)
-            return redirect("/music",mood)
+            print(mood)
+            # the first argument in url_for takes the function name, NOT the actual endpoint
+            return redirect(url_for("music", mood=songs[mood]))
 
     return render_template('index.html')
 
@@ -45,6 +53,3 @@ def music(mood):
 
 if __name__=='__main__':
     app.run(debug=True)
-
-
-
